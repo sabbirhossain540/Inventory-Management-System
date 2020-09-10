610377e6b5e1bddb616fee4f8f9cc23685afec7f
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-10 20:46:52 +0600

    Theams Installation

--- inventory/resources/js/app.js
+++ inventory/resources/js/app.js
@@ -6,7 +6,10 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 /**
  * The following block of code may be used to automatically register your

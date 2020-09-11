33c9a2b5e338bf92c23054dcb9eaf0e5e1355870
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-11 22:24:04 +0600

    create Helper

--- inventory/app/Http/Controllers/AuthController.php
+++ inventory/app/Http/Controllers/AuthController.php
@@ -95,7 +95,10 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'name' => auth()->user()->name,
+            'user_id' => auth()->user()->id,
+            'email' => auth()->user()->email,
         ]);
     }
 }

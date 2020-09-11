99afa59e094022cd1fe9afd3e80899a697655229
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-11 23:55:13 +0600

    login in Dashboard

--- inventory/resources/js/components/auth/login.vue
+++ inventory/resources/js/components/auth/login.vue
@@ -60,7 +60,10 @@
 		methods: {
 			login(){
 				 axios.post('/api/auth/login',this.form)
-				 .then(res => User.responseAfterLogin(res))
+				 .then(res => {
+				 	User.responseAfterLogin(res)
+				 	this.$router.push({name: 'home'})
+				 })
 				 .catch(error => console.log(error.response.data))
 			}
 		}
--- inventory/resources/js/routes.js
+++ inventory/resources/js/routes.js
@@ -2,8 +2,12 @@
 let login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 
+let home = require('./components/Home.vue').default;
+
 
 export const routes = [
   { path: '/', component: login, name: '/' },
-  { path: '/register', component: register, name:'register' }
+  { path: '/register', component: register, name:'register' },
+
+  { path: '/home', component: home, name:'home' }
 ]

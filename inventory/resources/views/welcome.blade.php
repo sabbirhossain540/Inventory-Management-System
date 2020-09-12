5069fb30d84aed0fcabca1bdb9ae41319f5e6ea7
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-12 19:08:56 +0600

    Logout Process

--- inventory/resources/views/welcome.blade.php
+++ inventory/resources/views/welcome.blade.php
@@ -303,10 +303,7 @@
                     Activity Log
                   </a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
-                  </a>
+                  <router-link to="/logout"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>Logout</router-link>
                 </div>
               </li>
             </ul>

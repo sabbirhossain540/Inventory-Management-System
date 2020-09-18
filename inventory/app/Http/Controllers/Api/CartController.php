313f50319f6450343fc8f835720e2701474a4f5c
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-18 17:59:32 +0600

    d

--- inventory/app/Http/Controllers/Api/CartController.php
+++ inventory/app/Http/Controllers/Api/CartController.php
@@ -10,14 +10,24 @@ class CartController extends Controller
 {
     public function addToCart(Request $request, $id){
     	$product = DB::table('products')->where('id', $id)->first();
-    	$data = array();
-    	$data['pro_id'] = $id;
-    	$data['pro_name'] = $product->product_name;
-    	$data['pro_quantity'] = 1;
-    	$data['product_price'] = $product->selling_price;
-    	$data['sub_total'] = $product->selling_price;
 
-    	DB::table('pos')->insert($data);
+    	$check = DB::table('pos')->where('pro_id', $id)->first();
+
+    	if($check){
+    		DB::table('pos')->where('pro_id', $id)->increment('pro_quantity');
+    	}else{
+    		$data = array();
+	    	$data['pro_id'] = $id;
+	    	$data['pro_name'] = $product->product_name;
+	    	$data['pro_quantity'] = 1;
+	    	$data['product_price'] = $product->selling_price;
+	    	$data['sub_total'] = $product->selling_price;
+
+	    	DB::table('pos')->insert($data);
+    	}
+
+
+    	
     }
 
     public function CartProduct(){
